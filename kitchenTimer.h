2d5749a81fbf063cbb8926f3c30148f75200ff15
VAleSh-Soft <v258@narod.ru>
2022-02-08 11:02:59 +0300

    version 0.1

--- kitchenTimer.h
+++ kitchenTimer.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <Arduino.h>
-#include <DS3231.h> 
+#include <DS3231.h>
 
 // #define USE_LIGHT_SENSOR // использовать или нет датчик света для регулировки яркости дисплея
 
@@ -8,9 +8,16 @@
 #define DISPLAY_MODE_SET_HOUR 1   // режим настройки часов
 #define DISPLAY_MODE_SET_MINUTE 2 // режим настройки минут
 
+#define BTN_FLAG_NONE 0 // флаг кнопки - ничего не делать
+#define BTN_FLAG_NEXT 1 // флаг кнопки - изменить значение
+#define BTN_FLAG_EXIT 2 // флаг кнопки - возврат в режим показа текущего времени
+
 #define BTN_SET_PIN 4
 #define BTN_UP_PIN 9
 #define BTN_DOWN_PIN 6
+#ifdef USE_LIGHT_SENSOR
+#define LIGHT_SENSOR_PIN A0
+#endif
 
 // ==== опрос кнопок =================================
 void checkButton();
@@ -22,6 +29,7 @@ void checkDownButton();
 void blink();
 void restartBlink();
 void returnToDefMode();
+void showTimeSetting();
 #ifdef USE_LIGHT_SENSOR
 void setBrightness();
 #endif
@@ -30,3 +38,11 @@ void setBrightness();
 // вывод времени на индикатор
 void showTime(DateTime dt, bool force = false);
 void showTime(byte hour, byte minute, bool force = false);
+// вывод на экран данных в режиме настройки времени
+void showTimeSettingData(byte hour, byte minute);
+// сохранение времени после настройки
+void saveTime(byte hour, byte minute);
+
+// ==== разное =======================================
+// изменение данных по клику кнопки с контролем выхода за предельное значение
+void checkData(byte &dt, byte max, bool toUp);
--- kitchenTimer.ino
+++ kitchenTimer.ino
@@ -5,18 +5,25 @@
 #include <shButton.h>
 #include <shTaskManager.h>
 
+// ==== настройки ====================================
+#define MIN_DISPLAY_BRIGHTNESS 1 // минимальная яркость дисплея, 1-7
+#define MAX_DISPLAY_BRIGHTNESS 7 // максимальная яркость дисплея, 1-7
+#define AUTO_EXIT_TIMEOUT 10     // время автоматического возврата в режим показа текущего времени из любых других режимов при отсутствии активности пользователя, секунд
+// ===================================================
+
 TM1637Display tm(11, 10); // CLK, DAT
 DS3231 clock;             // SDA - A4, SCL - A5
 RTClib RTC;
 
 #ifdef USE_LIGHT_SENSOR
-shTaskManager tasks(3); // создаем список задач
+shTaskManager tasks(4); // создаем список задач
 #else
-shTaskManager tasks(2); // создаем список задач
+shTaskManager tasks(3); // создаем список задач
 #endif
 
 shHandle blink_timer;            // таймер блинка
 shHandle return_to_default_mode; // таймер автовозврата в режим показа времени из любого режима настройки
+shHandle set_time_mode;          // таймер настройки времени
 #ifdef USE_LIGHT_SENSOR
 shHandle light_sensor_guard; // отслеживание показаний датчика света
 #endif
@@ -27,6 +34,9 @@ bool blink_flag = false;
 // ==== класс кнопок с предварительной настройкой ====
 class ktButton : public shButton
 {
+private:
+  byte _flag = BTN_FLAG_NONE;
+
 public:
   ktButton(byte button_pin) : shButton(button_pin)
   {
@@ -34,6 +44,16 @@ public:
     shButton::setLongClickMode(LCM_ONLYONCE);
     shButton::setVirtualClickOn(true);
     shButton::setDebounce(50);
+  }
+
+  byte getBtnFlag()
+  {
+    return (_flag);
+  }
+
+  void setBtnFlag(byte flag)
+  {
+    _flag = flag;
   }
 
   byte getButtonState()
@@ -70,14 +90,60 @@ void checkButton()
 
 void checkSetButton()
 {
+  switch (btnSet.getButtonState())
+  {
+  case BTN_DOWN:
+  case BTN_DBLCLICK:
+
+    break;
+  case BTN_ONECLICK:
+    switch (displayMode)
+    {
+    case DISPLAY_MODE_SET_HOUR:
+    case DISPLAY_MODE_SET_MINUTE:
+      btnSet.setBtnFlag(BTN_FLAG_NEXT);
+      break;
+    }
+    break;
+  case BTN_LONGCLICK:
+    switch (displayMode)
+    {
+    case DISPLAY_MODE_SHOW_TIME:
+      displayMode = DISPLAY_MODE_SET_HOUR;
+      break;
+    case DISPLAY_MODE_SET_HOUR:
+    case DISPLAY_MODE_SET_MINUTE:
+      btnSet.setBtnFlag(BTN_FLAG_EXIT);
+      break;
+    }
+    break;
+  }
 }
 
 void checkUpButton()
 {
+  switch (btnUp.getButtonState())
+  {
+  case BTN_ONECLICK:
+
+    break;
+  case BTN_LONGCLICK:
+
+    break;
+  }
 }
 
 void checkDownButton()
 {
+  switch (btnDown.getButtonState())
+  {
+  case BTN_ONECLICK:
+
+    break;
+  case BTN_LONGCLICK:
+
+    break;
+  }
 }
 
 // ===================================================
@@ -102,8 +168,66 @@ void restartBlink()
 
 void returnToDefMode()
 {
-  displayMode = DISPLAY_MODE_SHOW_TIME;
+  switch (displayMode)
+  {
+  case DISPLAY_MODE_SET_HOUR:
+  case DISPLAY_MODE_SET_MINUTE:
+    btnSet.setBtnFlag(BTN_FLAG_EXIT);
+    break;
+  }
   tasks.stopTask(return_to_default_mode);
+}
+
+void showTimeSetting()
+{
+  static bool time_checked = false;
+  static byte curHour = 0;
+  static byte curMinute = 0;
+
+  if (!tasks.getTaskState(set_time_mode))
+  {
+    tasks.startTask(set_time_mode);
+    tasks.startTask(return_to_default_mode);
+    restartBlink();
+    time_checked = false;
+  }
+
+  if (!time_checked)
+  {
+    DateTime dt = RTC.now();
+    curHour = dt.hour();
+    curMinute = dt.minute();
+  }
+  // опрос кнопок =====================
+  switch (btnSet.getBtnFlag())
+  {
+  case BTN_FLAG_NEXT:
+    checkData(displayMode, DISPLAY_MODE_SET_MINUTE, true);
+    btnSet.setBtnFlag(BTN_FLAG_NONE);
+    if (displayMode == DISPLAY_MODE_SHOW_TIME)
+    {
+      if (time_checked)
+      {
+        saveTime(curHour, curMinute);
+        time_checked = false;
+      }
+      tasks.stopTask(set_time_mode);
+      return;
+    }
+    break;
+  case BTN_FLAG_EXIT:
+    btnSet.setBtnFlag(BTN_FLAG_NONE);
+    displayMode = DISPLAY_MODE_SHOW_TIME;
+    if (time_checked)
+    {
+      saveTime(curHour, curMinute);
+      time_checked = false;
+    }
+    tasks.stopTask(set_time_mode);
+    return;
+  }
+  // вывод данных на индикатор ======
+  showTimeSettingData(curHour, curMinute);
 }
 
 #ifdef USE_LIGHT_SENSOR
@@ -111,7 +235,7 @@ void setBrightness()
 {
   static word b;
   b = (b * 2 + analogRead(LIGHT_SENSOR_PIN)) / 3;
-  byte c = (b < 300) ? 1 : 7;
+  byte c = (b < 300) ? MIN_DISPLAY_BRIGHTNESS : MAX_DISPLAY_BRIGHTNESS;
   tm.setBrightness(c);
 }
 #endif
@@ -138,6 +262,41 @@ void showTime(byte hour, byte minute, bool force = false)
   }
 }
 
+void showTimeSettingData(byte hour, byte minute)
+{
+  uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
+  data[0] = tm.encodeDigit(hour / 10);
+  data[1] = tm.encodeDigit(hour % 10);
+  data[2] = tm.encodeDigit(minute / 10);
+  data[3] = tm.encodeDigit(minute % 10);
+  // если наступило время блинка и кнопки Up/Down не нажата, то стереть соответствующие разряды; при нажатых кнопках Up/Down во время изменения данных ничего не мигает
+  if (!blink_flag && !btnUp.isButtonClosed() && !btnDown.isButtonClosed())
+  {
+    byte i = (displayMode == DISPLAY_MODE_SET_HOUR) ? 0 : 2;
+    data[i] = 0x00;
+    data[i + 1] = 0x00;
+  }
+  tm.setSegments(data);
+}
+
+void saveTime(byte hour, byte minute)
+{
+  clock.setHour(hour);
+  clock.setMinute(minute);
+  clock.setSecond(0);
+}
+
+// ===================================================
+// изменение данных по клику кнопки с контролем выхода за предельное значение
+void checkData(byte &dt, byte max, bool toUp)
+{
+  (toUp) ? dt++ : dt--;
+  if (dt > max)
+  {
+    dt = (toUp) ? 0 : max;
+  }
+}
+
 // ===================================================
 void setup()
 {
@@ -152,11 +311,12 @@ void setup()
   btnDown.setLongClickTimeout(100);
 
   blink_timer = tasks.addTask(500, blink);
-  return_to_default_mode = tasks.addTask(10000, returnToDefMode, false);
+  return_to_default_mode = tasks.addTask(AUTO_EXIT_TIMEOUT * 1000, returnToDefMode, false);
+  set_time_mode = tasks.addTask(100, showTimeSetting, false);
 #ifdef USE_LIGHT_SENSOR
   light_sensor_guard = tasks.addTask(100, setBrightness);
 #else
-  tm.setBrightness(7);
+  tm.setBrightness(MAX_DISPLAY_BRIGHTNESS);
 #endif
 }
 
@@ -168,6 +328,13 @@ void loop()
   {
   case DISPLAY_MODE_SHOW_TIME:
     showTime(RTC.now());
+    break;
+  case DISPLAY_MODE_SET_HOUR:
+  case DISPLAY_MODE_SET_MINUTE:
+    if (!tasks.getTaskState(set_time_mode))
+    {
+      showTimeSetting();
+    }
     break;
   default:
     break;
